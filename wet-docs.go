20df8cea3ccf0961d009df2602b739200481ea5b
Lucas Torri <lucastorri@gmail.com>
2015-02-22 16:43:59 +0100

    glueing parts together

--- wet-docs.go
+++ wet-docs.go
@@ -12,29 +12,7 @@ func main() {
     runtime.GOMAXPROCS(runtime.NumCPU())
 
     file := "/Users/lucastorri/Work/wet-stream/CC-MAIN-20141224185923-00096-ip-10-231-17-201.ec2.internal.warc.wet.gz"
-    wr, err := wet.FromGZip(file)
-    wch := wr.Channel()
 
-    if err != nil {
-        panic(err)
-    }
-
-    done := make(chan bool)
-
-    go func() {
-        defer func() {
-            done <- true
-        }()
-        for wet := range wch {
-            if wet.Err == io.EOF {
-                fmt.Println("end")
-            } else if wet.Err != nil {
-                panic(err)
-            } else {
-                // fmt.Println(wet.Entry.Version)
-            }
-        }
-    }()
 
 
     q, err := workq.NewQueue("local=" + file + "," + file)
@@ -42,13 +20,33 @@ func main() {
     if err != nil {
         panic(err)
     }
-
-    fch := q.Channel()
+    done := make(chan bool)
     go func() {
+        fch := q.Channel()
+        defer func() {
+            done <- true
+        }()
         for f := range fch {
             fmt.Println(f.Filepath())
+            wr, err := wet.FromGZip(f.Filepath())
+            if err == nil {
+                wch := wr.Channel()
+                for wet := range wch {
+                    if wet.Err == io.EOF {
+                        fmt.Println("!")
+                    } else if wet.Err != nil {
+                        panic(err)
+                    } else {
+                        fmt.Print(".")
+                    }
+                }
+                f.Ack()
+            } else {
+                f.Nack()
+            }
         }
     }()
+
 
     <-done
 }
