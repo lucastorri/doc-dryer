e0c335c1c44105ed4165b1b34b25ed7f91436fc3
Lucas Torri <lucastorri@gmail.com>
2015-02-23 17:18:56 +0100

    download file to be processed

--- queue/rabbitmq.go
+++ queue/rabbitmq.go
@@ -2,22 +2,21 @@ package queue
 
 import (
     "github.com/streadway/amqp"
+    "net/http"
+    "io"
+    "io/ioutil"
 )
 
 
+var queueName = "work-queue"
+
+//TODO close itself after N minute idle
 type rabbitMQ struct {
     conn *amqp.Connection
     ch *amqp.Channel
     channel chan Work
     stop chan bool
 }
-
-type rabbitMQWork struct {
-    rmq *rabbitMQ
-    delivery amqp.Delivery
-}
-
-var queueName = "work-queue"
 
 func (rmq *rabbitMQ) Channel() (channel <-chan Work) {
     return rmq.channel
@@ -37,9 +36,30 @@ func (rmq *rabbitMQ) init() (err error) {
             for {
                 select {
                     case delivery := <- deliveries:
-                        //TODO to finish, have to download the file, put in a tmp dir, and pass the filepath on
-                        //http://stackoverflow.com/questions/11692860/how-can-i-efficiently-download-a-large-file-using-go
-                        work := &rabbitMQWork { rmq, delivery }
+                        work := &rabbitMQWork { rmq, delivery, "" }
+                        var err error
+
+                        file, err := ioutil.TempFile("", "doc-dryer-wet-part-")
+                        if err != nil {
+                            work.Nack()
+                            continue
+                        }
+                        defer file.Close()
+
+                        work.filepath = file.Name()
+                        res, err := http.Get(string(delivery.Body))
+                        if err != nil {
+                            work.Nack()
+                            continue
+                        }
+                        defer res.Body.Close()
+
+                        _, err = io.Copy(file, res.Body)
+                        if err != nil {
+                            work.Nack()
+                            continue
+                        }
+
                         rmq.channel <- work
                     case <-rmq.stop:
                         break
@@ -55,8 +75,14 @@ func (rmq *rabbitMQ) Close() {
 }
 
 
+type rabbitMQWork struct {
+    rmq *rabbitMQ
+    delivery amqp.Delivery
+    filepath string
+}
+
 func (w *rabbitMQWork) Filepath() string {
-    return string(w.delivery.Body)
+    return w.filepath
 }
 
 func (w *rabbitMQWork) Ack() error {
@@ -66,6 +92,7 @@ func (w *rabbitMQWork) Ack() error {
 func (w *rabbitMQWork) Nack() error {
     return w.delivery.Nack(false, true)
 }
+
 
 func setup(url, queue string) (conn *amqp.Connection, ch *amqp.Channel, err error) {
     if conn, err = amqp.Dial(url); err == nil {
